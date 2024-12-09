b8aa64d31ce9a6302873317b35ed22d616abeddb
Lime712 <122940036+Lime712@users.noreply.github.com>
2024-12-09 17:31:24 +1300

    sempiternity

---
+++ src/sempiternity/SempiternityMod.java
@@ -0,0 +1,24 @@
+package sempiternity;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+
+public class SempiternityMod extends Mod{
+
+    public SempiternityMod(){
+        
+        });
+    }
+
+    @Override
+    public void loadContent(){
+        Log.info("Loading some example content.");
+    }
+
+}
